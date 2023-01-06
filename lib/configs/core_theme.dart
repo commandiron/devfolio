2900fffb57a15386b0e374ac3852e88d6532a3a7
commandiron <commandiron@gmail.com>
2023-01-06 17:48:17 +0300

    .

--- lib/configs/core_theme.dart
+++ lib/configs/core_theme.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 const fontFamily = 'Poppins';
 
 final themeLight = ThemeData(
-  primaryColorLight: const Color(0xffC0392B),
+  primaryColorLight: const Color(0xffF48B0B),
   brightness: Brightness.light,
-  primaryColor: const Color(0xffC0392B),
+  primaryColor: const Color(0xffF48B0B),
   highlightColor: Colors.black,
   canvasColor: Colors.white,
   fontFamily: fontFamily,
@@ -20,16 +20,16 @@ final themeLight = ThemeData(
 
 final themeDark = ThemeData(
   brightness: Brightness.dark,
-  primaryColorDark: const Color(0xffC0392B),
-  primaryColor: const Color(0xffC0392B),
-  highlightColor: const Color(0xffC0392B),
+  primaryColorDark: const Color(0xffF48B0B),
+  primaryColor: const Color(0xffF48B0B),
+  highlightColor: const Color(0xffF48B0B),
   canvasColor: Colors.white,
   fontFamily: fontFamily,
   splashColor: Colors.transparent,
   backgroundColor: Colors.grey[800],
   scaffoldBackgroundColor: Colors.black,
   colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.red).copyWith(
-    secondary: const Color(0xffC0392B),
+    secondary: const Color(0xffF48B0B),
     brightness: Brightness.dark,
   ),
 );
